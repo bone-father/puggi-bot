d803c7eae299c162d33bfddbacd3e3d3932d3986
Wendy Ou <84300214+bone-father@users.noreply.github.com>
2022-01-22 19:43:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import discord
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='-')
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
 
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if "mitten" in message.content.lower():
         await message.channel.send("Well yes, I'd love to cum in your mittens thanks for asking")
 
-client.run('OTM0MzU2MDYyNDg2ODAyNDUy.Yeu45A._FoadnBB34yHvk4vL6Xrvtwqs9U')
+    await bot.process_commands(message)
+
+@bot.command()
+async def nicole(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/934608180896485437/934608395783249980/nicole.jpg')
+
+bot.run('OTM0MzU2MDYyNDg2ODAyNDUy.Yeu45A._FoadnBB34yHvk4vL6Xrvtwqs9U')
