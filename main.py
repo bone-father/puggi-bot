1784fd5e4a43ea4f50bcba1590930f3a327adccb
Wendy Ou <84300214+bone-father@users.noreply.github.com>
2022-01-22 19:55:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,4 +21,7 @@ async def on_message(message):
 async def nicole(ctx):
     await ctx.send('https://cdn.discordapp.com/attachments/934608180896485437/934608395783249980/nicole.jpg')
 
+async def zach(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/934608180896485437/934612074032668725/zach.jpg')
+
 bot.run('OTM0MzU2MDYyNDg2ODAyNDUy.Yeu45A._FoadnBB34yHvk4vL6Xrvtwqs9U')
