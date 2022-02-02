0e2ed782d5ae9faa1f9f30294e8d7653db9666d9
Wendy Ou <84300214+bone-father@users.noreply.github.com>
2022-02-01 23:48:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    mommy = random.randint(1, 100)
+    mommy = random.randint(1, 420)
 
     if mommy == 69:
         await bot.get_channel(938249020088922152).send(str(mommy) + " <@700092415910084608>")
